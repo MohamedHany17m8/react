2dec739f588e02d280f5e5f4a1f61aaffc312225
MohamedHany17m8 <mhany6921@gmail.com>
2025-01-26 01:17:35 +0200

    states challenge

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import React from "react";
 import FormComponent from "./FormComponent";
+import { useState } from "react";
 const App = () => {
+  const [player, setPlayer] = useState({
+    firstName: "Ranjani",
+    lastName: "Shettar",
+    score: 10,
+  });
+
+  function handlePlusClick() {
+    setPlayer({
+      ...player,
+      score: player.score + 1,
+    });
+  }
+
+  function handleFirstNameChange(e) {
+    setPlayer({
+      ...player,
+      firstName: e.target.value,
+    });
+  }
+
+  function handleLastNameChange(e) {
+    setPlayer({
+      ...player,
+      lastName: e.target.value,
+    });
+  }
   return (
-    <div>
-      <div>
-        <h1>React Form with Select and Radio Buttons</h1>
-        <FormComponent />
-      </div>
-    </div>
+    <>
+      <label>
+        Score: <b>{player.score}</b>{" "}
+        <button onClick={handlePlusClick}>+1</button>
+      </label>
+      <label>
+        First name:
+        <input value={player.firstName} onChange={handleFirstNameChange} />
+      </label>
+      <label>
+        Last name:
+        <input value={player.lastName} onChange={handleLastNameChange} />
+      </label>
+    </>
   );
 };
 
